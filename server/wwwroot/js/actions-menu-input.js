cbc69808cdd96bb5dd774db50868043a1de6c74b
yazayalks <86235524+yazayalks@users.noreply.github.com>
2022-02-10 23:11:49 +0700

    reload site

---
+++ server/wwwroot/js/actions-menu-input.js
@@ -0,0 +1,3 @@
+back.onclick = function() {
+    document.location.href = "../pages/selection.html";
+};
---
+++ server/wwwroot/js/actions-selection.js
@@ -0,0 +1,13 @@
+authorization.onclick = function() {
+    document.location.href = "../pages/authorization.html";
+};
+
+registration.onclick = function() {
+    document.location.href = "../pages/registration.html";
+};
+
+observation.onclick = function() {
+    document.location.href = "../pages/observation.html";
+};
+
+
