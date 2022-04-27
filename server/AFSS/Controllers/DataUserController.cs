bd15545326fd5010fb65aed48f0385209c7cef9d
Kirill Lysak <lysak@arl>
2022-04-27 18:19:14 +0700

    add ViewModel

--- server/AFSS/Controllers/DataUserController.cs
+++ server/AFSS/Controllers/DataUserController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AFSS.Controllers
 {
+    [Route("[controller]")]
     public class DataUserController : Controller
     {
         public IActionResult DataUser()
@@ -11,14 +12,24 @@ namespace AFSS.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult EditName(EditNameViewModel model)
+        public IActionResult EditName(DataUserViewModel model)
         {
             if (ModelState.IsValid)
             {
                 return View("DataUser");
             }
-            return View(model);
+            return View(model.Name);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditEmail(EmailViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("DataUser");
+            }
+            return View(model.Email);
         }
 
-        }
+    }
 }
